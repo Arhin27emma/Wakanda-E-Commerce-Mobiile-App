740a1e615cb7ceb9725ce04423930abfe783083e
Emmanuel Arhinful <ttc@Scholar.local>
2024-01-03 10:41:38 +0000

    Updating chat

--- lib/screens/chatPage/chatPage.dart
+++ lib/screens/chatPage/chatPage.dart
@@ -1,3 +1,4 @@
+import 'package:app/screens/chatPage/chatUsers.dart';
 import 'package:app/screens/chatPage/messagePage.dart';
 import 'package:flutter/material.dart';
 
@@ -162,50 +163,3 @@ class _ChatPageState extends State<ChatPage> {
   }
 }
 
-class ChatUsers extends StatelessWidget {
-  const ChatUsers({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return const Row(                            
-      children: [
-        CircleAvatar(
-          radius: 30,
-          backgroundImage: AssetImage("assets/images/catShoe1.png"),
-        ),
-    
-        Padding(
-          padding: EdgeInsets.all(8.0),
-          child: Column(
-            
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text("Isaac", style: TextStyle(color: Colors.indigo, fontSize: 18, fontWeight: FontWeight.bold),),
-                  SizedBox(width: 175,),
-                  Text("12:30", style: TextStyle(color: Colors.indigo, fontSize: 15,),),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text("This is a messag...", style: TextStyle(color: Colors.indigo, fontSize: 16),),
-                  SizedBox(width: 100,),
-                  CircleAvatar(
-                    minRadius: 8,
-                    backgroundColor: Color.fromARGB(255, 124, 216, 127),
-                    child: Center(child: Text("1", style: TextStyle(color: Colors.white, fontSize: 18,),)),
-                  ),
-                ],
-              ),
-                                        ],
-                                      ),
-        )
-          ],
-        );
-  }
-}
---
+++ lib/screens/chatPage/chatUsers.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+
+class ChatUsers extends StatelessWidget {
+  const ChatUsers({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(                            
+      children: [
+        CircleAvatar(
+          radius: 30,
+          backgroundImage: AssetImage("assets/images/catShoe1.png"),
+        ),
+    
+        Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Column(
+            
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Isaac", style: TextStyle(color: Colors.indigo, fontSize: 18, fontWeight: FontWeight.bold),),
+                  SizedBox(width: 175,),
+                  Text("12:30", style: TextStyle(color: Colors.indigo, fontSize: 15,),),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("This is a messag...", style: TextStyle(color: Colors.indigo, fontSize: 16),),
+                  SizedBox(width: 100,),
+                  CircleAvatar(
+                    minRadius: 8,
+                    backgroundColor: Color.fromARGB(255, 124, 216, 127),
+                    child: Center(child: Text("1", style: TextStyle(color: Colors.white, fontSize: 18,),)),
+                  ),
+                ],
+              ),
+                                        ],
+                                      ),
+        )
+          ],
+        );
+  }
+}
