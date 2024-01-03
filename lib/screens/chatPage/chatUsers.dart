389e3226e38ae7cb57c5d09ea8ee463c042f2a06
Emmanuel Arhinful <ttc@Scholar.local>
2024-01-03 11:45:48 +0000

    Update chatUsers

--- lib/screens/chatPage/chatUsers.dart
+++ lib/screens/chatPage/chatUsers.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class ChatUsers extends StatelessWidget {
   const ChatUsers({
     super.key,
@@ -8,43 +7,67 @@ class ChatUsers extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Row(                            
+    return const Row(
       children: [
         CircleAvatar(
           radius: 30,
           backgroundImage: AssetImage("assets/images/catShoe1.png"),
         ),
-    
         Padding(
           padding: EdgeInsets.all(8.0),
           child: Column(
-            
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text("Isaac", style: TextStyle(color: Colors.indigo, fontSize: 18, fontWeight: FontWeight.bold),),
-                  SizedBox(width: 175,),
-                  Text("12:30", style: TextStyle(color: Colors.indigo, fontSize: 15,),),
+                  Text(
+                    "Isaac",
+                    style: TextStyle(
+                        color: Colors.indigo,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    width: 175,
+                  ),
+                  Text(
+                    "12:30",
+                    style: TextStyle(
+                      color: Colors.indigo,
+                      fontSize: 15,
+                    ),
+                  ),
                 ],
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text("This is a messag...", style: TextStyle(color: Colors.indigo, fontSize: 16),),
-                  SizedBox(width: 100,),
+                  Text(
+                    "This is a messag...",
+                    style: TextStyle(color: Colors.indigo, fontSize: 16),
+                  ),
+                  SizedBox(
+                    width: 100,
+                  ),
                   CircleAvatar(
                     minRadius: 8,
                     backgroundColor: Color.fromARGB(255, 124, 216, 127),
-                    child: Center(child: Text("1", style: TextStyle(color: Colors.white, fontSize: 18,),)),
+                    child: Center(
+                        child: Text(
+                      "1",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                      ),
+                    )),
                   ),
                 ],
               ),
-                                        ],
-                                      ),
+            ],
+          ),
         )
-          ],
-        );
+      ],
+    );
   }
 }
